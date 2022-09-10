40f27dd0171715e5a6534e1f4db6c11ad38b6963
daviiid99 <daviiideveloper@gmail.com>
2022-09-10 22:14:38 +0200

    msm8916-common: vendor: Update firmware to latest J510FNXXS3BTI6

--- j5xnlte/j5xnlte-vendor.mk
+++ j5xnlte/j5xnlte-vendor.mk
@@ -17,4 +17,5 @@
 PRODUCT_COPY_FILES += \
     vendor/samsung/j5xnlte/proprietary/vendor/lib/hw/sensors.vendor.msm8916.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors.vendor.msm8916.so \
     vendor/samsung/j5xnlte/proprietary/vendor/lib/libsec-ril-dsds.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsec-ril-dsds.so \
-    vendor/samsung/j5xnlte/proprietary/vendor/lib/libsec-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsec-ril.so
+    vendor/samsung/j5xnlte/proprietary/vendor/lib/libsec-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsec-ril.so \
+    vendor/samsung/j5xnlte/proprietary/vendor/firmware/libpn548ad_fw.so:$(TARGET_COPY_OUT_VENDOR)/firmware/libpn548ad_fw.so
